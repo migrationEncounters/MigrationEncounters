1a65e65249890365e9af35d201181ea514b18ebb
Luis Aguilar <luis.aguilar@enroutesystems.com>
2020-07-17 19:28:54 -0500

    WIP

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
+// Import libraries
 import React from 'react'
 import PropTypes from 'prop-types'
 
 const Footer = (props) => (
-    <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy; Disclaimer <a href="https://html5up.net">Link</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
-    </footer>
+  <footer id="footer" style={props.timeout ? {display: 'none'} : {}} >
+    <p className="copyright">&copy; Disclaimer <a href="https://html5up.net">Link</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
+  </footer>
 )
 
 Footer.propTypes = {
