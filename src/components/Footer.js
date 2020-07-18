8abc74f159ac13a2c6fe3a4c51d6bfa5cc1ddaa3
Luis Aguilar <luis.aguilar@enroutesystems.com>
2020-07-17 19:50:51 -0500

    changes to the footer and bg overlay

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,38 @@ import PropTypes from 'prop-types'
 
 const Footer = (props) => (
   <footer id="footer" style={props.timeout ? {display: 'none'} : {}} >
-    <p className="copyright">&copy; Disclaimer <a href="https://html5up.net">Link</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
+		<ul className="icons">
+			<li>
+				<a
+					href="/"
+					className="icon fa-twitter"
+				>
+					<span className="label">Twitter</span>
+				</a>
+			</li>
+			<li>
+				<a 
+					href="/" 
+					className="icon fa-facebook"
+				>
+					<span className="label">Facebook</span>
+				</a>
+			</li>
+			<li>
+				<a 
+					href="/" 
+					className="icon fa-instagram"
+				>
+					<span className="label">Instagram</span>
+				</a>
+			</li>
+		</ul>
+		
+    <p className="copyright">
+			&copy; Disclaimer <a href="/">Link</a>. 
+			Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a>. 
+			Using the base starter template by <a href="https://html5up.net">html5</a>
+		</p>
   </footer>
 )
 
