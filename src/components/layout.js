a743b2e16bef11280a1aaa27e7bae68f64fd93dc
Luis Aguilar <luis.aguilar.pena123@gmail.com>
2020-07-20 14:09:19 -0500

    removed the static query and cleaned layout.j

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,62 +1,41 @@
+// Import libraries
 import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
 
+// Import styles
 import '../assets/scss/main.scss'
 
-const Layout = ({ children, location }) => {
-
-  let content;
-
-  if (location && location.pathname === '/') {
-    content = (
-      <div>
-        {children}
-      </div>
-    )
-  } else {
-    // for pages other than the home page
-    content = (
-      <div id="wrapper" className="page">
+const Layout = props => {
+  const { children, location } = props;
+  return (
+    <Fragment>
+      <Helmet
+        title="Migration Encounters"
+        meta={[
+          { name: 'Migration Encounters description', content: 'Sample content ME' }, // to change
+          { name: 'keywords ME', content: 'sample, something Migration Encounters' },
+        ]}
+      >
+        <html lang="en" />
+      </Helmet>
+      {location && location.pathname === '/' ?
         <div>
           {children}
         </div>
-      </div>
-    )
-  }
-
-  return (
-    <StaticQuery
-      query={graphql`
-        query SiteTitleQuery {
-          site {
-            siteMetadata {
-              title
-            }
-          }
-        }
-      `}
-      render={data => (
-        <Fragment>
-          <Helmet
-            title={data.site.siteMetadata.title}
-            meta={[
-              { name: 'Migration Encounters description', content: 'Sample content ME' }, // to change
-              { name: 'keywords ME', content: 'sample, something Migration Encounters' },
-            ]}
-          >
-            <html lang="en" />
-          </Helmet>
-          {content}
-        </Fragment>
-      )}
-    />
+      : // below is where I wanna add the page only menu bar
+        <div id="wrapper" className="page">
+          <div>
+            {children}
+          </div>
+        </div>
+      }
+    </Fragment>
   )
-}
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
